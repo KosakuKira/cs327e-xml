ef0a6c3cc06baa755864bfeddc563d8bc282a523
mandygabriel <mandygabriel@users.noreply.github.com>
2014-02-14 17:37:58 -0600

    basic run, with file name input

---
+++ RunXML.py
@@ -0,0 +1,12 @@
+#----------
+# imports
+#----------
+
+from XML import xml_solve
+from xml.etree.ElementTree import Element, fromstring
+
+#----------
+# Main Run
+#----------
+
+xml_solve("ElementTree.xml")
