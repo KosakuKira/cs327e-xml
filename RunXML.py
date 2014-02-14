fb6d62067045a7c3b130a8bcd0a0151bd42543e7
mandygabriel <mandygabriel@users.noreply.github.com>
2014-02-14 17:48:25 -0600

    to work w stdin

--- RunXML.py
+++ RunXML.py
@@ -1,12 +1,22 @@
+#!/usr/bin/env python3
+
+"""
+To run the program
+    % python RunXML.py < RunXML.in > RunXML.out
+    % chmod ugo+x RunXML.py
+    % RunXML.py < RunXML.in > RunXML.out
+"""
+
 #----------
 # imports
 #----------
 
 from XML import xml_solve
 from xml.etree.ElementTree import Element, fromstring
+import sys
 
 #----------
 # Main Run
 #----------
 
-xml_solve("ElementTree.xml")
+xml_solve(sys.stdin)
